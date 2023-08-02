f2d15a019b6a998a6e6a44c1e00b490050f77703
ItsAlphaHelix <mitak4000@gmail.com>
2023-08-02 11:59:57 +0300

    Added in service authorization-interception

---
+++ Portfolio.UI/src/app/services/authorization.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    console.log('The intercept is called!')
+    return next.handle(request);
+  }
+}
